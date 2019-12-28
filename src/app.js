26a24e7547b408b54072340d9d2264521809fa76
lpdenver <lpdenver@googlemail.com>
2019-12-28 23:55:57 +0500

    Module 4: Creating a Data Access Library

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
+const { accounts,  users,  writeJSON} = require('./data');
+
 
 const app = express();
 
@@ -12,11 +14,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.urlencoded({ extended: true }));
 
-const accountData = fs.readFileSync(path.join(__dirname, 'json/accounts.json'), { encoding: 'UTF8' });
-const accounts = JSON.parse(accountData);
-
-const userData = fs.readFileSync(path.join(__dirname, 'json/users.json'), { encoding: 'UTF8' });
-const users = JSON.parse(userData);
 
 app.get('/', (req, res) => {
   res.render('index', { title: 'Account Summary', accounts });
@@ -41,8 +38,7 @@ app.post('/transfer', (req, res) => {
   const { from, to, amount } = req.body;
   accounts[from].balance -= parseInt(amount);
   accounts[to].balance += parseInt(amount);
-  const accountsJSON = JSON.stringify(accounts);
-  fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, 'UTF8');
+  writeJSON();
   res.render('transfer', { message: "Transfer Completed" });
 });
 app.get('/payment', (req, res) => {
@@ -52,8 +48,7 @@ app.post('/payment', (req, res) => {
   const {amount} = req.body;
   accounts.credit.balance -=parseInt(amount);
   accounts.credit.available +=parseInt(amount);
-  const accountsJSON = JSON.stringify(accounts);
-  fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, 'UTF8');
+  writeJSON();
   res.render('payment', { message: "Payment Successful", account: accounts.credit });
 });
 
