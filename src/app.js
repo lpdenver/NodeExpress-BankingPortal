7cd8ea7b31c007a0a0da8f7cb5415b625dd08f75
lpdenver <lpdenver@googlemail.com>
2019-12-28 23:04:00 +0500

    Module2:  File Handling and Routing

--- src/app.js
+++ src/app.js
@@ -10,10 +10,30 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
-  res.render('index',{title:'Index'});
-})
+const accountData = fs.readFileSync(path.join(__dirname, 'json/accounts.json'), { encoding: 'UTF8' });
+const accounts = JSON.parse(accountData);
 
-app.listen(port,()=>{
+const userData = fs.readFileSync(path.join(__dirname, 'json/users.json'), { encoding: 'UTF8' });
+const users = JSON.parse(userData);
+
+app.get('/', (req, res) => {
+  res.render('index', { title: 'Account Summary', accounts });
+})
+app.get('/savings', (req, res) => {
+  res.render('account', { account: accounts.savings });
+})
+app.get('/checking', (req, res) => {
+  res.render('account', { account: accounts.checking });
+})
+app.get('/credit', (req, res) => {
+  res.render('account', { account: accounts.credit });
+})
+app.get('/profile', (req, res) => {
+  const [user] = users;
+  res.render('profile', { user });
+});
+
+
+app.listen(port, () => {
   console.log(`PS Project Running on port ${port}!`);
 })
