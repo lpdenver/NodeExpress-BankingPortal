9dd1798bf2a89a190121b661e7d4a1e7aab6f4c5
lpdenver <lpdenver@googlemail.com>
2019-12-28 23:47:09 +0500

    Module3: Handling Form Data

--- src/app.js
+++ src/app.js
@@ -10,6 +10,8 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(express.urlencoded({ extended: true }));
+
 const accountData = fs.readFileSync(path.join(__dirname, 'json/accounts.json'), { encoding: 'UTF8' });
 const accounts = JSON.parse(accountData);
 
@@ -31,6 +33,29 @@ app.get('/credit', (req, res) => {
 app.get('/profile', (req, res) => {
   const [user] = users;
   res.render('profile', { user });
+});
+app.get('/transfer', (req, res) => {
+  res.render('transfer');
+});
+app.post('/transfer', (req, res) => {
+  console.log(req.body);
+  const { from, to, amount } = req.body;
+  accounts[from].balance -= parseInt(amount);
+  accounts[to].balance += parseInt(amount);
+  const accountsJSON = JSON.stringify(accounts);
+  fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, 'UTF8');
+  res.render('transfer', { message: "Transfer Completed" });
+});
+app.get('/payment', (req, res) => {
+  res.render('payment', { account: accounts.credit });
+});
+app.post('/payment', (req, res) => {
+  const {amount} = req.body;
+  accounts.credit.balance -=parseInt(amount);
+  accounts.credit.available +=parseInt(amount);
+  const accountsJSON = JSON.stringify(accounts);
+  fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, 'UTF8');
+  res.render('payment', { message: "Payment Successful", account: accounts.credit });
 });
 
 
